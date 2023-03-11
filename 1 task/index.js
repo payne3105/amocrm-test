1c5077555521a791b395991ec85f91d3a612134f
payne3105 <payne3105@gmail.com>
2023-03-11 21:09:36 +0300

    Solved first task

--- 1 task/index.js
+++ 1 task/index.js
@@ -1,24 +1,53 @@
-const inputEl = document.querySelector('input');
-const buttonEl = document.querySelector('button');
-const timerEl = document.querySelector('span');
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
 
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    const timer = setInterval(() => {
+      let hoursRes = Math.floor((seconds % (60 * 60 * 24)) / (60 * 60));
+      let minutesRes = Math.floor((seconds % (60 * 60)) / 60);
+      let secondsRes = Math.floor(seconds % 60);
+
+      if (hoursRes.toString().length == 1) {
+        hoursRes = "0" + hoursRes;
+      }
+      if (minutesRes.toString().length == 1) {
+        minutesRes = "0" + minutesRes;
+      }
+      if (secondsRes.toString().length == 1) {
+        secondsRes = "0" + secondsRes;
+      }
+
+      timerEl.textContent = `${hoursRes}:${minutesRes}:${secondsRes}`;
+      console.log(seconds);
+      seconds--;
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(timer);
+      alert("time is out!");
+      timerEl.textContent = "hh:mm:ss";
+    }, seconds * 1000 + 1000);
+  };
 };
 
 const animateTimer = createTimerAnimator();
 
-inputEl.addEventListener('input', () => {
+inputEl.addEventListener("input", () => {
   // Очистите input так, чтобы в значении
   // оставались только числа
+  if (!(Number(inputEl.value) || inputEl.value == null)) {
+    alert("enter only numbers");
+  }
 });
 
-buttonEl.addEventListener('click', () => {
+buttonEl.addEventListener("click", () => {
   const seconds = Number(inputEl.value);
 
   animateTimer(seconds);
 
-  inputEl.value = '';
+  inputEl.value = "";
 });
